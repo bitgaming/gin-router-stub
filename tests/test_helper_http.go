70017c5278e2a0ecdea32c33ae9043fff829967f
Tony Kung <tony.kung@bitgamelabs.com>
2016-07-15 13:56:39 +0800

    allowing register router url patterns

--- tests/test_helper_http.go
+++ tests/test_helper_http.go
@@ -1,54 +1,56 @@
 package tests
 
 import (
-	"fmt"
-	"reflect"
-	"errors"
-	"encoding/json"
 	"bytes"
-	"strings"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"net/http/httptest"
-	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/contrib/sessions"
+	"reflect"
+	"strings"
 )
 
 // request handling func type to replace gin.HandlerFunc
 type RequestFunc func(*gin.Context)
+
 // response handling func type
 type ResponseFunc func(*httptest.ResponseRecorder)
 
 type RequestConfig struct {
-	Method string
-	Path string
-	Body string
-	Headers map[string]string
+	Method      string
+	RoutePath   string
+	Path        string
+	Body        string
+	Headers     map[string]string
 	Middlewares []gin.HandlerFunc
-	Handler RequestFunc
-	Finaliser ResponseFunc 
+	Handler     RequestFunc
+	Finaliser   ResponseFunc
 }
 
 func RunRequest(rc RequestConfig) {
 	gin.SetMode(gin.TestMode)
 	r := gin.New()
-	
+
 	if rc.Middlewares != nil && len(rc.Middlewares) > 0 {
 		for _, mw := range rc.Middlewares {
 			r.Use(mw)
 		}
 	}
-	
+
 	qs := ""
 	if strings.Contains(rc.Path, "?") {
 		ss := strings.Split(rc.Path, "?")
 		rc.Path = ss[0]
 		qs = ss[1]
 	}
-	
+
 	body := bytes.NewBufferString(rc.Body)
 
 	req, _ := http.NewRequest(rc.Method, rc.Path, body)
-	
+
 	if len(qs) > 0 {
 		req.URL.RawQuery = qs
 	}
@@ -58,88 +60,94 @@ func RunRequest(rc RequestConfig) {
 			req.Header.Set(k, v)
 		}
 	} else if rc.Method == "POST" || rc.Method == "PUT" {
-		if strings.HasPrefix(rc.Body, "{"){
+		if strings.HasPrefix(rc.Body, "{") {
 			req.Header.Set("Content-Type", "application/json")
 		} else {
 			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 		}
 	}
-	
-	r.Handle(rc.Method, rc.Path, func(c *gin.Context){
+
+	r.Handle(rc.Method, rc.RoutePath, func(c *gin.Context) {
 		//change argument if necessary here
 		rc.Handler(c)
 	})
-	
+
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
-	
+
 	if rc.Finaliser != nil {
 		rc.Finaliser(w)
 	}
 }
 
-func RunSimpleGet(path string, handler RequestFunc, reply ResponseFunc) {
+func RunSimpleGet(routePath string, path string, handler RequestFunc, reply ResponseFunc) {
 	rc := RequestConfig{
-		Method: "GET",
-		Path: path,
-		Handler: handler,
+		Method:    "GET",
+		RoutePath: routePath,
+		Path:      path,
+		Handler:   handler,
 		Finaliser: reply,
 	}
 	RunRequest(rc)
 }
 
-func RunSimplePost(path, body string, handler RequestFunc, reply ResponseFunc) {
+func RunSimplePost(routePath string, path, body string, handler RequestFunc, reply ResponseFunc) {
 	rc := RequestConfig{
-		Method: "POST",
-		Path: path,
-		Body: body,
-		Handler: handler,
+		Method:    "POST",
+		RoutePath: routePath,
+		Path:      path,
+		Body:      body,
+		Handler:   handler,
 		Finaliser: reply,
 	}
 	RunRequest(rc)
 }
 
-func RunGetWithHeaders(path string, headers map[string]string, handler RequestFunc, reply ResponseFunc) {
+func RunGetWithHeaders(routePath string, path string, headers map[string]string, handler RequestFunc, reply ResponseFunc) {
 	rc := RequestConfig{
-		Method: "GET",
-		Path: path,
-		Headers: headers,
-		Handler: handler,
+		Method:    "GET",
+		RoutePath: routePath,
+		Path:      path,
+		Headers:   headers,
+		Handler:   handler,
 		Finaliser: reply,
 	}
 	RunRequest(rc)
 }
 
-func RunGetWithMiddlewares(path string, mws []gin.HandlerFunc, handler RequestFunc, reply ResponseFunc) {
+func RunGetWithMiddlewares(routePath string, path string, mws []gin.HandlerFunc, handler RequestFunc, reply ResponseFunc) {
 	rc := RequestConfig{
-		Method: "GET",
-		Path: path,
+		Method:      "GET",
+		RoutePath:   routePath,
+		Path:        path,
 		Middlewares: mws,
-		Handler: handler,
-		Finaliser: reply,
+		Handler:     handler,
+		Finaliser:   reply,
 	}
 	RunRequest(rc)
 }
 
-func RunPostWithMiddlewares(path, body string, mws []gin.HandlerFunc, handler RequestFunc, reply ResponseFunc) {
+func RunPostWithMiddlewares(routePath string, path, body string, mws []gin.HandlerFunc, handler RequestFunc, reply ResponseFunc) {
 	rc := RequestConfig{
-		Method: "POST",
-		Path: path,
-		Body: body,
+		Method:      "POST",
+		RoutePath:   routePath,
+		Path:        path,
+		Body:        body,
 		Middlewares: mws,
-		Handler: handler,
-		Finaliser: reply,
+		Handler:     handler,
+		Finaliser:   reply,
 	}
 	RunRequest(rc)
 }
 
-func RunWithMiddlewares(method, path, body string, handler RequestFunc, reply ResponseFunc) {
+func RunWithMiddlewares(routePath string, method, path, body string, handler RequestFunc, reply ResponseFunc) {
 	rc := RequestConfig{
-		Method: method,
-		Path: path,
+		Method:      method,
+		RoutePath:   routePath,
+		Path:        path,
 		Middlewares: MiddleWares(),
-		Handler: handler,
-		Finaliser: reply,
+		Handler:     handler,
+		Finaliser:   reply,
 	}
 	if method == "POST" || method == "PUT" {
 		rc.Body = body
@@ -147,14 +155,15 @@ func RunWithMiddlewares(method, path, body string, handler RequestFunc, reply Re
 	RunRequest(rc)
 }
 
-func RunWithHeaderMiddlewares(method, path, body string, headers map[string]string, handler RequestFunc, reply ResponseFunc) {
+func RunWithHeaderMiddlewares(routePath string, method, path, body string, headers map[string]string, handler RequestFunc, reply ResponseFunc) {
 	rc := RequestConfig{
-		Method: method,
-		Path: path,
-		Headers: headers,
+		Method:      method,
+		RoutePath:   routePath,
+		Path:        path,
+		Headers:     headers,
 		Middlewares: MiddleWares(),
-		Handler: handler,
-		Finaliser: reply,
+		Handler:     handler,
+		Finaliser:   reply,
 	}
 	if method == "POST" || method == "PUT" {
 		rc.Body = body
@@ -164,13 +173,13 @@ func RunWithHeaderMiddlewares(method, path, body string, headers map[string]stri
 
 func MiddleWares() []gin.HandlerFunc {
 	return []gin.HandlerFunc{
-		sessions.Sessions("session",sessions.NewCookieStore([]byte("12345678"))),
+		sessions.Sessions("session", sessions.NewCookieStore([]byte("12345678"))),
 	}
 }
 
-func VerifyResponse(r *httptest.ResponseRecorder, code int, data map[string]interface{}) (bool,error) {
+func VerifyResponse(r *httptest.ResponseRecorder, code int, data map[string]interface{}) (bool, error) {
 	if r.Code != code {
-		s := fmt.Sprintf("Code returned:%d != expected:%d",r.Code,code)
+		s := fmt.Sprintf("Code returned:%d != expected:%d", r.Code, code)
 		return false, errors.New(s)
 	}
 	var rd map[string]interface{}
@@ -179,7 +188,7 @@ func VerifyResponse(r *httptest.ResponseRecorder, code int, data map[string]inte
 		return false, err
 	}
 	if !reflect.DeepEqual(rd, data) {
-		s := fmt.Sprintf("R.Body:%+v",rd)
+		s := fmt.Sprintf("R.Body:%+v", rd)
 		return false, errors.New(s)
 	}
 	return true, nil
